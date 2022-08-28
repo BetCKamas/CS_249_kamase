38c4723810c5f8bc5f2523c2b77a104f9fd62255
Dr. Michael Reale <realemj@sunypoly.edu>
2022-08-28 14:56:58 -0400

    Minor commit

--- src/edu/realemj/Example/HowdyPrinter.java
+++ src/edu/realemj/Example/HowdyPrinter.java
@@ -2,7 +2,7 @@ package edu.realemj.Example;
 
 public class HowdyPrinter {
     public static void main(String [] args) {
-        System.out.println("HENLO AND GREETS FRENS!");
+        System.out.println("HENLO AND GREETS FRENS!  HOW ARE YOU?");
     }
 
     public static String getPassword() {
