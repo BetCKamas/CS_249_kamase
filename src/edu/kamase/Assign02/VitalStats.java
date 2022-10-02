68a33b8c08775db2beef77f681bc5ead6d945c36
Betsy Kamas <kamase@sunypoly.edu>
2022-10-02 13:11:21 -0400

    Assign02 work

--- src/edu/kamase/Assign02/VitalStats.java
+++ src/edu/kamase/Assign02/VitalStats.java
@@ -1,7 +1,24 @@
 package edu.kamase.Assign02;
 
+import java.util.*;
+
 public class VitalStats {
     public static void main (String [] args){
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter name:");
+        String firstName = input.next();
+        String lastName = input.next();
+
+        System.out.println("Enter height in inches:");
+        int height = input.nextInt();
+
+        System.out.println("Enter weight in pounds:");
+
+        System.out.println("NAME: " + lastName + ", " + firstName);
+
+        input.close();
         
     }
     
