a80c20c318723266b081e6dbc9cd5fad816d8d2d
Betsy Kamas <kamase@sunypoly.edu>
2022-10-23 18:30:02 -0400

    Assign03

---
+++ src/edu/kamase/Assign03/LetterPrinter.java
@@ -0,0 +1,13 @@
+package edu.kamase.Assign03;
+
+import java.util.*;
+
+public class LetterPrinter {
+    public static Letterhead askForLetterData(Scanner input){
+
+    }
+
+    public static void main(String [] args){
+        
+    }
+}
